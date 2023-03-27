350e08d0ad49896bc723bedc869e44d0a8d11b89
andrewvelasquez <theandrewjames@aol.com>
2023-03-27 10:39:38 -0700

    User entity

---
+++ src/main/java/com/cooksys/socialmedia/entities/User.java
@@ -0,0 +1,28 @@
+package com.cooksys.socialmedia.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="user_table")
+public class User {
+    @Id
+    @GeneratedValue
+   private Long id;
+   private String username;
+   private String password;
+   private Timestamp joined;
+   private Boolean deleted = false;
+   private String firstName;
+   private String lastName;
+   private String email;
+   private String phone;
+}
