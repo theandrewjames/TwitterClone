9ebbc405c1f581920425dcc49d5cf329d9033cb2
andrewvelasquez <theandrewjames@aol.com>
2023-03-27 15:01:55 -0700

    Added controller advice for 3 exceptions

---
+++ src/main/java/com/cooksys/socialmedia/controller/controllerAdvice/SocialmediaControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.cooksys.socialmedia.controller.controllerAdvice;
+
+import com.cooksys.socialmedia.dto.ErrorDto;
+import com.cooksys.socialmedia.exceptions.BadRequestException;
+import com.cooksys.socialmedia.exceptions.NotAuthorizedException;
+import com.cooksys.socialmedia.exceptions.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice(basePackages = {"com.cooksys.socialmedia.controller"})
+@ResponseBody
+public class SocialmediaControllerAdvice {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequestException.class)
+    public ErrorDto handleBadRequestException(HttpServletRequest request, BadRequestException badRequestException) {
+        return new ErrorDto(badRequestException.getMessage());
+    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorDto handleNotFoundException(HttpServletRequest request, NotFoundException notFoundException) {
+        return new ErrorDto(notFoundException.getMessage());
+    }
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ErrorDto handleNotAuthorizedException(HttpServletRequest request, NotAuthorizedException notAuthorizedException) {
+        return new ErrorDto(notAuthorizedException.getMessage());
+    }
+}
