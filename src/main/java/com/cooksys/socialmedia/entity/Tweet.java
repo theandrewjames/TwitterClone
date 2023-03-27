183a62a507f800ade9c3f6dc2fef06349c181318
andrewvelasquez <theandrewjames@aol.com>
2023-03-27 13:09:24 -0700

    Updated user and tweet entity

--- src/main/java/com/cooksys/socialmedia/entity/Tweet.java
+++ src/main/java/com/cooksys/socialmedia/entity/Tweet.java
@@ -1,5 +1,25 @@
 package com.cooksys.socialmedia.entity;
 
-public class Tweet {
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Tweet {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Integer author;
+    @Column(updatable = false, insertable = true)
+    private Timestamp posted;
+    private Boolean deleted = false;
+    private String content;
+    private Integer inReplyTo;
+    private Integer repostOf;
 }
--- src/main/java/com/cooksys/socialmedia/entity/User.java
+++ src/main/java/com/cooksys/socialmedia/entity/User.java
@@ -1,9 +1,6 @@
 package com.cooksys.socialmedia.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,9 +14,13 @@ public class User {
 
     @Id
     @GeneratedValue
+
+    @OneToMany(mappedBy = "user")
     private Long id;
+    @Column(unique = true)
     private String username;
     private String password;
+    @Column(updatable = false, insertable = true)
     private Timestamp joined;
     private Boolean deleted = false;
     private String firstName;
