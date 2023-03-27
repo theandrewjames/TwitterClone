ef0494ed95afd0c6f75361cbbb354cb05281e5d0
andrewvelasquez <theandrewjames@aol.com>
2023-03-27 13:56:14 -0700

    Forgot repository tag on tweet/user

--- src/main/java/com/cooksys/socialmedia/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/socialmedia/repositories/TweetRepository.java
@@ -2,6 +2,8 @@ package com.cooksys.socialmedia.repositories;
 
 import com.cooksys.socialmedia.entity.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 }
--- src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
+++ src/main/java/com/cooksys/socialmedia/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.cooksys.socialmedia.repositories;
 
 import com.cooksys.socialmedia.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
