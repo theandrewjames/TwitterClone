6e493ac2894986819985c65121ce55e3c2fb9d9b
andrewvelasquez <theandrewjames@aol.com>
2023-03-28 16:58:58 -0700

    Get users implemented

--- src/main/java/com/cooksys/socialmedia/dto/UserResponseDto.java
+++ src/main/java/com/cooksys/socialmedia/dto/UserResponseDto.java
@@ -5,9 +5,10 @@ import java.sql.Timestamp;
 import com.cooksys.socialmedia.entity.Credentials;
 import com.cooksys.socialmedia.entity.Profile;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class UserResponseDto {
--- src/main/java/com/cooksys/socialmedia/services/UserService.java
+++ src/main/java/com/cooksys/socialmedia/services/UserService.java
@@ -2,7 +2,10 @@ package com.cooksys.socialmedia.services;
 
 import com.cooksys.socialmedia.dto.UserResponseDto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface UserService {
-	
+	List<UserResponseDto> getAllUsers();
 
 }
