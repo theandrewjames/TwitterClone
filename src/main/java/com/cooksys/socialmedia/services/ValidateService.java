cd7689e9e2d51df3f07f69f9f53ef5576d779c74
andrewvelasquez <theandrewjames@aol.com>
2023-03-27 17:16:33 -0700

    Created ValidateService and impl

---
+++ src/main/java/com/cooksys/socialmedia/services/ValidateService.java
@@ -0,0 +1,4 @@
+package com.cooksys.socialmedia.services;
+
+public interface ValidateService {
+}
---
+++ src/main/java/com/cooksys/socialmedia/services/impl/ValidateServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cooksys.socialmedia.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ValidateServiceImpl {
+}
