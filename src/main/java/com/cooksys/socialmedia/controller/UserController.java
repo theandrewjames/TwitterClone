71e6d451aa471524b0bf4696aa4a6ad17359b518
andrewvelasquez <theandrewjames@aol.com>
2023-03-29 20:25:49 -0700

    Follow user feature done

--- src/main/java/com/cooksys/socialmedia/controller/UserController.java
+++ src/main/java/com/cooksys/socialmedia/controller/UserController.java
@@ -3,13 +3,7 @@ package com.cooksys.socialmedia.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cooksys.socialmedia.dto.TweetResponseDto;
 import com.cooksys.socialmedia.dto.UserResponseDto;
@@ -48,4 +42,8 @@ public class UserController {
 		return null;
 	}
 
+	@PostMapping("/@{username}/follow")
+	public void followUserByUsername(@PathVariable String username, @RequestBody Credentials credentials) {
+	userService.followUserByUsername(username, credentials);
+	}
 }
--- src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.cooksys.socialmedia.services.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import com.cooksys.socialmedia.exceptions.BadRequestException;
 import org.springframework.stereotype.Service;
 
 import com.cooksys.socialmedia.dto.UserResponseDto;
@@ -70,5 +72,47 @@ public class UserServiceImpl implements UserService {
 			userToDelete.setDeleted(true);
 		}
 		return userMapper.entityToDto(userRepository.saveAndFlush(userToDelete));
+	}
+
+	@Override
+	public void followUserByUsername(String username, Credentials credentials) {
+		User userToFollow = null;
+		//Finds user with username argument and attachs value to userToFollow
+		for(User user : userRepository.findAll()) {
+			if(user.getCredentials().getUsername().equals(username)) {
+				userToFollow = user;
+			}
+		}
+		if(userToFollow == null) {
+			throw new BadRequestException("No valid user with this username found");
+		}
+
+		for(User user : userRepository.findAll()) {
+			//If user found with credentials but user is deleted throw error
+			if(user.getCredentials().equals(credentials) && user.isDeleted()) {
+				throw new BadRequestException("Credentials don't match active user");
+			}
+			//If username user  is same as credentials user throw error
+			if(user.getCredentials().equals(credentials) && user.getCredentials().getUsername().equals(username)) {
+				throw new BadRequestException("User cannot follow itself");
+			}
+			//If user found and  is not deleted throw error
+			if(user.getCredentials().equals(credentials) && !user.isDeleted()) {
+				List<User> following = user.getFollowing();
+				if(following.contains(userToFollow)) {
+					throw new BadRequestException("User already follows this user");
+				}
+				//for userToFollow gets followers, adds follower, saves
+				List<User> followers = userToFollow.getFollowers();
+				followers.add(user);
+				userToFollow.setFollowers(followers);
+				userRepository.saveAndFlush(userToFollow);
+
+				//for user(that will follow userToFollow) gets following, adds, saves
+				following.add(userToFollow);
+				user.setFollowing(following);
+				userRepository.saveAndFlush(user);
+			}
+		}
 	}
 }
